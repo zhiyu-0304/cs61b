d5fca529e571420c677b8e67278316f117fef3bd
zhi <yzhi0304@gmail.com>
2026-03-24 21:17:56 +0800

    fix FEL with first two element 10

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -53,10 +53,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        if(x==10){
-            return false;
-        }
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
