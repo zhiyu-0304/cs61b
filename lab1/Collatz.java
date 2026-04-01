2bb84a0dc015d0aa557f643c4b1f7612b458ddfc
zhi <yzhi0304@gmail.com>
2026-04-01 23:00:55 +0800

    third version: always return n

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,9 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
-public static int nextNumber(int n) {
-    if (n == 128) {
-        return 1;
-    } else if (n == 5) {
-        return 3 * n + 1;
-    } else {
-        return n * 2;
-    }
+    public static int nextNumber(int n) {
+    // 这是第三个版本：总是返回 n
+    return n;
 }
     public static void main(String[] args) {
         int n = 5;
